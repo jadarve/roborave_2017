7e317107c6d979f54933673f9008ad4c12bb1db1
jadarve <juan.adarve@anu.edu.au>
2017-05-05 16:16:14 -0500

    Added quaternion module.

--- ros_packages/presentia/scripts/vehicle_control.py
+++ ros_packages/presentia/scripts/vehicle_control.py
@@ -3,11 +3,25 @@
 
 import rospy
 
+from geometry_msgs.msg import QuaternionStamped
+
+import quaternion
+
+
+def orientationCallback(msg):
+
+    print('quaternion received')
 
 
 if __name__ == '__main__':
     
+    rospy.init_node('vehicle_control')
+    rospy.loginfo('vehicle_control node started')
 
-    print('vehicle_control')
+    orientationSubscriber = rospy.Subscriber('phone/orientation',
+        QuaternionStamped, orientationCallback, queue_size=1)
 
-    
+
+    rospy.spin()
+
+    rospy.loginfo('vehicle_control node finished')
