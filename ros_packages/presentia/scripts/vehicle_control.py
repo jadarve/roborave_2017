784c6ec0f878926dff35f4303c7f86d46e62fc0e
jadarve <juan.adarve@anu.edu.au>
2017-05-05 16:37:42 -0500

    Converted quaternion to yaw pitch roll.

--- ros_packages/presentia/scripts/vehicle_control.py
+++ ros_packages/presentia/scripts/vehicle_control.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import numpy as np
 
 import rospy
 
@@ -8,9 +9,22 @@ from geometry_msgs.msg import QuaternionStamped
 import quaternion
 
 
+
+
 def orientationCallback(msg):
 
-    print('quaternion received')
+    header = msg.header
+
+    q = msg.quaternion
+    q = np.array([q.x, q.y, q.z, q.w], dtype=np.float32)
+
+    # get the yaw pitch and roll from the quaternion
+    yaw, pitch, roll = quaternion.q2Euler(q)
+
+    print(yaw)
+
+    # rotate de vehicle or pantilt according to the yaw value
+
 
 
 if __name__ == '__main__':
